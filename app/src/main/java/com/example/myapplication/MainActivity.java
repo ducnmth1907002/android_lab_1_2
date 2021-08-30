4568fe94bfae4404eef9c3bd876bace4a33eb43a
Your Name <kythuat.arigatou@gmail.com>
2021-08-30 21:38:10 +0700

    lab 3.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvContact;
+    private List<ContactModel> listContacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        lvContact = (ListView) findViewById(R.id.lvContact);
+        ContactAdapter adapter = new ContactAdapter(listContacts, this);
+        lvContact.setAdapter(adapter);
+
+        lvContact.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                ContactModel contactModel = listContacts.get(position);
+                Toast.makeText(MainActivity.this, contactModel.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initData() {
+        ContactModel contact = new ContactModel("Nguyen Duc", "0123456789", R.drawable.ic_avatar1);
+        listContacts.add(contact);
+        contact = new ContactModel("Nguyen Duc 1", "0123456789", R.drawable.ic_avatar1);
+        listContacts.add(contact);
     }
 }
