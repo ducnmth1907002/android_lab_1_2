61c6df8d2cbc0091915d95ea71c0de2936f7c4d9
Your Name <kythuat.arigatou@gmail.com>
2021-09-13 18:39:44 +0700

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager simpleViewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+// get the reference of ViewPager and TabLayout
+        simpleViewPager = (ViewPager) findViewById(R.id.simpleViewPager);
+        tabLayout = (TabLayout) findViewById(R.id.simpleTabLayout);
+// Create a new Tab named "First"
+        TabLayout.Tab firstTab = tabLayout.newTab();
+        firstTab.setText("First"); // set the Text for the first Tab
+        firstTab.setIcon(R.drawable.ic_launcher_background); // set an icon for the
+// first tab
+        tabLayout.addTab(firstTab); // add  the tab at in the TabLayout
+// Create a new Tab named "Second"
+        TabLayout.Tab secondTab = tabLayout.newTab();
+        secondTab.setText("Second"); // set the Text for the second Tab
+        secondTab.setIcon(R.drawable.ic_launcher_background); // set an icon for the second tab
+        tabLayout.addTab(secondTab); // add  the tab  in the TabLayout
+
+        PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        simpleViewPager.setAdapter(adapter);
+// addOnPageChangeListener event change the tab on slide
+        simpleViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                FirstFragment tab1 = new FirstFragment();
+                return tab1;
+            case 1:
+                SecondFragment tab2 = new SecondFragment();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
