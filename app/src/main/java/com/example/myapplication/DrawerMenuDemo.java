45412121706068a654217287f64520827fe38041
Your Name <kythuat.arigatou@gmail.com>
2021-09-20 19:59:18 +0700

    drawer

--- app/src/main/java/com/example/myapplication/DrawerMenuDemo.java
+++ app/src/main/java/com/example/myapplication/DrawerMenuDemo.java
@@ -9,21 +9,31 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class DrawerMenuDemo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class DrawerMenuDemo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NavigationBarView.OnItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    FrameLayout frameLayout;
+    FragmentOne fragmentOne;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_menu_demo);
+        initView();
+    }
 
+    private void initView() {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.drawer_nav_view);
         toolbar = findViewById(R.id.drawer_toolbar);
+        bottomNavigationView = findViewById(R.id.drawer_bottom_view);
 
         navigationView.bringToFront();
         setSupportActionBar(toolbar);
@@ -32,6 +42,15 @@ public class DrawerMenuDemo extends AppCompatActivity implements NavigationView.
                         R.string.open_menu, R.string.close_menu);
         drawerLayout.addDrawerListener(toggle);
         navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+        //gán fragment vào frame layout
+        frameLayout = findViewById(R.id.main_content);
+        fragmentOne = new FragmentOne();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_content, fragmentOne, FragmentOne.class.getName())
+                .commit();
     }
 
     @Override
